34a9f854fc120dcd64d0e8f2ed05d82362abae87
eugenishef <eugenishef@yandex.ru>
2024-06-03 21:38:48 +0300

    первая версия

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,22 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
 public class Film {
+    private int id;
+    @NotNull(message = "Название не может быть пустым")
+    private String name;
+    @Size(max = 200, message = "Максимальная длина описания — 200 символов")
+    private String description;
+    @PastOrPresent(message = "Дата релиза должна быть не позднее текущей даты")
+    private LocalDate releaseDate;
+    @Min(value = 1, message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
