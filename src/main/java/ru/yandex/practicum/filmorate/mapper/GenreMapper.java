09a0796c2116f30d4b03917632b60dd1d7996b5a
Elena.M <el.muss@yandex.ru>
2024-07-20 15:45:42 +0300

    Добавление функции add-most-populars

--- src/main/java/ru/yandex/practicum/filmorate/mapper/GenreMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/GenreMapper.java
@@ -1,10 +1,10 @@
 package ru.yandex.practicum.filmorate.mapper;
 
-import lombok.experimental.UtilityClass;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.dto.GenreDto;
 import ru.yandex.practicum.filmorate.model.Genre;
 
-@UtilityClass
+@Component
 public final class GenreMapper {
     public static GenreDto modelToDto(Genre genre) {
         return GenreDto.builder()
